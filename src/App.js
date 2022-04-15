5b7b8e00cdcd90b26af4d482f29080a9b554a557
LanAleCoder <peter24alexander@gmail.com>
2022-04-14 20:58:52 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from "react";
 
-function App() {
+
+const App = () => {
+  const [colorHex, setColorHex] = useState("#dddddd");
+  const randomColorHex = () => {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    setColorHex(randomColor);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundColor: `${colorHex}`}}>
+      
+      <div className="container__counter-father">
+      <div className="container__counter-child">
+        
+      <h1>{colorHex}</h1>
+      <div className="container__counter-buttons">        
+      <button type="button" className="container__counter-button increase" onClick = {randomColorHex}>Random</button>
+      <button type="button" className="container__counter-button reset" onClick = {() => navigator.clipboard.writeText(colorHex)}>Copy</button>
+      </div>
+      </div>
+    </div>
+
     </div>
   );
 }
